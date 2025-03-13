d86bfc4a77dc623f50c4d2501e268f3f58c96d4a
Josh Kramer <kramer@nyu.edu>
2025-03-13 09:06:22 -0400

    added another WordArea Component

--- client/src/components/LetterRow.jsx
+++ client/src/components/LetterRow.jsx
@@ -1,14 +1,17 @@
 import LetterBox from "./LetterBox";
 
-const LetterRow = () => {
+const LetterRow = ( {wordarray} ) => {
     const rowStyle = {
         display: "flex",
         gap: "8px",
         padding: "16px",
     };
 
+   const letters = wordarray
+
     // maybe put the logic here?  
     // I can run the checkWord() and see assign each returned value?
+    // maybe send an array in, {["W", "G"], ["O", "G"]} etc
 
     return (
         <div style={rowStyle}>
---
+++ client/src/components/WordArea.jsx
@@ -0,0 +1,9 @@
+import LetterRow from "./LetterRow";
+
+//LetterRow needs to take in either a bunch of props, or one prop formatted as an array
+
+const WordArea = () => {
+    return (
+        <LetterRow />
+    )
+}
